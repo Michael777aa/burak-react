a275aaf5911801b71b19bbc6577ce24368f6950e
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-07-09 17:13:49 +0900

    fix: organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,33 +1,21 @@
 import React from "react";
 import "../css/app.css";
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
 import { ProductsPage } from "./screens/productsPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { UserPage } from "./screens/userPage";
 import { HomePage } from "./screens/homepage";
+import { HomeNavbar } from "./components/headers/HomeNavbat";
+import { OtherNavbar } from "./components/headers/OtherNavbar";
+import { Footer } from "./components/footer";
 
 function App() {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">HomePage</Link>
-          </li>
-          <li>
-            <Link to="/products">ProductPage</Link>
-          </li>
-          <li>
-            <Link to="/orders">OrdersPage</Link>
-          </li>
-          <li>
-            <Link to="/member-page">UserPage</Link>
-          </li>
-        </ul>
-      </nav>
+  const location = useLocation();
+  console.log(location);
 
-      {/* A <Switch> looks through its children <Route>s and
-        renders the first one that matches the current URL. */}
+  return (
+    <>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
       <Switch>
         <Route path="/products">
           <ProductsPage />
@@ -42,7 +30,8 @@ function App() {
           <HomePage />
         </Route>
       </Switch>
-    </div>
+      <Footer />
+    </>
   );
 }
 
