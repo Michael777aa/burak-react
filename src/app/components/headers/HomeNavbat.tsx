6e818b78db9a115cea4b758c17fef0f72a749e04
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-07-10 17:44:11 +0900

    fix: homeNavbar menu part refactoring

--- src/app/components/headers/HomeNavbat.tsx
+++ src/app/components/headers/HomeNavbat.tsx
@@ -5,28 +5,15 @@ export function HomeNavbar() {
   const authMember = null;
   return (
     <div className="home-navbar">
-      <Container sx={{ mt: "55px", height: "642px" }}>
-        <Stack
-          sx={{ height: "50px" }}
-          flexDirection={"row"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-        >
+      <Container className="navbar-container">
+        <Stack className="menu">
           <Box>
             <NavLink to="/">
-              <img
-                src="/icons/burak.svg"
-                style={{ width: "125px", height: "30px" }}
-              />
+              <img src="/icons/burak.svg" className="brand-logo" />
             </NavLink>
           </Box>
 
-          <Stack
-            flexDirection={"row"}
-            justifyContent={"space-between"}
-            minWidth={"700px"}
-            alignItems={"center"}
-          >
+          <Stack className="links">
             <Box className={"hover-line"}>
               <NavLink to="/" activeClassName={"underline"}>
                 Home
@@ -59,15 +46,16 @@ export function HomeNavbar() {
             {/* BASKET */}
             {!authMember ? (
               <Box>
-                <Button
-                  variant="contained"
-                  style={{ background: "#3776cc", color: "#f8f8ff" }}
-                >
+                <Button variant="contained" className="login-button">
                   Login
                 </Button>
               </Box>
             ) : (
-              <img />
+              <img
+                className="user-avatar"
+                src="/icons/default-user.svg"
+                aria-haspopup={"true"}
+              />
             )}
           </Stack>
         </Stack>
