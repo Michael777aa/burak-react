0164e45b5d88399efc171d4b74ae5fa6cd61d0d5
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-08-01 00:26:04 +0900

    feat: create homePage Redux configurations

--- src/app/screens/homepage/index.tsx
+++ src/app/screens/homepage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
@@ -6,8 +6,15 @@ import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUser";
 import Events from "./Events";
 import "../../../css/home.css";
+import { log } from "console";
 
 export default function HomePage() {
+  // Selector:Store => Data
+
+  useEffect(() => {
+    // Backend server data request => Data
+    // Slice:Data => Store
+  }, []);
   return (
     <div className={"homepage"}>
       <Statistics />
