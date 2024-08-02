300fca62de045526c8f20a750c310562578ed833
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-08-02 20:10:44 +0900

    feat: retrieve top users and modify ActiveUsers component

--- src/app/screens/homepage/ActiveUser.tsx
+++ src/app/screens/homepage/ActiveUser.tsx
@@ -4,14 +4,20 @@ import Card from "@mui/joy/Card";
 import CardOverflow from "@mui/joy/CardOverflow";
 import AspectRatio from "@mui/joy/AspectRatio";
 
-const activeUsers = [
-  { productName: "Martin", imagePath: "/img/martin.webp" },
-  { productName: "Justin", imagePath: "/img/justin.webp" },
-  { productName: "Rose", imagePath: "/img/rose.webp" },
-  { productName: "Nusret", imagePath: "/img/nusret.webp" },
-];
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { retrieveTopUsers } from "./selector";
+import { serverApi } from "../../../lib/config";
+import { Member } from "../../../lib/types/member";
+
+//REDUX SLICE SELECTOR
+
+const topUsersRetriever = createSelector(retrieveTopUsers, (topUsers) => ({
+  topUsers,
+}));
 
 export default function ActiveUsers() {
+  const { topUsers } = useSelector(topUsersRetriever);
   return (
     <div className="active-users-frame">
       <Container>
@@ -19,20 +25,21 @@ export default function ActiveUsers() {
           <Box className="category-title">Active Users</Box>
           <Stack className="cards-frame">
             <CssVarsProvider>
-              {activeUsers.length !== 0 ? (
-                activeUsers.map((ele, index) => {
+              {topUsers.length !== 0 ? (
+                topUsers.map((member: Member, index) => {
+                  const imagePath = `${serverApi}/${member.memberImage}`;
                   return (
-                    <Card key={index} className="card">
+                    <Card key={member._id} className="card">
                       <CardOverflow>
                         <AspectRatio ratio="1">
-                          <img src={ele.imagePath} alt={ele.productName} />
+                          <img src={imagePath} />
                         </AspectRatio>
                       </CardOverflow>
 
                       <Stack className="info">
                         <Stack flexDirection="row">
                           <Typography className="title">
-                            {ele.productName}
+                            {member.memberNick}
                           </Typography>
                         </Stack>
                       </Stack>
