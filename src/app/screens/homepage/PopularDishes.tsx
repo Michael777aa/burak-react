137d9b21cc473b5fb62f8ae0d575f2f2dd0b3936
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-08-02 19:07:01 +0900

    feat: develop product service business logic to retreive data from backend server

--- src/app/screens/homepage/PopularDishes.tsx
+++ src/app/screens/homepage/PopularDishes.tsx
@@ -9,27 +9,35 @@ import VisibilityIcon from "@mui/icons-material/Visibility";
 import CardOverflow from "@mui/joy/CardOverflow";
 import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 
-const list = [
-  { productName: "Lavash", imagePath: "/img/lavash.webp" },
-  { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
-  { productName: "Kebab", imagePath: "/img/kebab.webp" },
-  { productName: "Kebab", imagePath: "/img/kebab-fresh.webp" },
-];
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { retrievePopularDishes } from "./selector";
+import { serverApi } from "../../../lib/config";
+import { Product } from "../../../lib/types/product";
+
+//REDUX SLICE SELECTOR
+
+const popularDishesRetriever = createSelector(
+  retrievePopularDishes,
+  (popularDishes) => ({ popularDishes })
+);
 
 export default function PopularDishes() {
+  const { popularDishes } = useSelector(popularDishesRetriever);
   return (
     <div className={"popular-dishes-frame"}>
       <Container>
         <Stack className={"popular-section"}>
           <Box className={"category-title"}>Popular Dishes</Box>
           <Stack className={"cards-frame"}>
-            {list.length !== 0 ? (
-              list.map((ele, index) => {
+            {popularDishes.length !== 0 ? (
+              popularDishes.map((ele: Product) => {
+                const imagePath = `${serverApi}/${ele.productImages[0]}`;
                 return (
-                  <CssVarsProvider key={index}>
+                  <CssVarsProvider key={ele._id}>
                     <Card className={"card"}>
                       <CardCover>
-                        <img src={ele.imagePath} alt={ele.productName} />
+                        <img src={imagePath} />
                       </CardCover>
                       <CardCover className={"card-cover"} />
                       <CardContent sx={{ justifyContent: "flex-end" }}>
@@ -53,6 +61,7 @@ export default function PopularDishes() {
                               display: "flex",
                             }}
                           >
+                            {ele.productViews}
                             <VisibilityIcon
                               sx={{ fontSize: 25, marginLeft: "5px" }}
                             />
@@ -70,7 +79,8 @@ export default function PopularDishes() {
                         }}
                       >
                         <Typography textColor="neutral.300">
-                          <DescriptionOutlinedIcon /> This is a delicious meal
+                          <DescriptionOutlinedIcon />
+                          {ele.productDesc}
                         </Typography>
                       </CardOverflow>
                     </Card>
--- src/app/screens/homepage/index.tsx
+++ src/app/screens/homepage/index.tsx
@@ -6,72 +6,37 @@ import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUser";
 import Events from "./Events";
 import "../../../css/home.css";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
 import { setPopularDishes } from "./slice";
-import { retrievePopularDishes } from "./selector";
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
 
 //REDUX SLICE SELECTOR
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
 });
-const popularDishesRetriever = createSelector(
-  retrievePopularDishes,
-  (popularDishes) => ({ popularDishes })
-);
 
 export default function HomePage() {
   const { setPopularDishes } = actionDispatch(useDispatch());
-  const { popularDishes } = useSelector(popularDishesRetriever);
+
   // Selector:Store => Data
 
   useEffect(() => {
     // Backend server data request => Data
-
-    const result = [
-      {
-        _id: "669fa8ab97c4d6969ec049d7",
-        productStatus: "PROCESS",
-        productCollection: "DISH",
-        productName: "shashlik",
-        productPrice: 14,
-        productLeftCount: 100,
-        productSize: "LARGE",
-        productVolume: "1",
-        productDesc: "good",
-        productImages: [
-          "uploads/products/c3a0312d-ee42-49ac-96de-35e4df61e18e.pdf",
-          "uploads/products/6d77bc27-a589-4e15-814f-ac34d69f2c36.jpg",
-        ],
-        productViews: 0,
-        createdAt: "2024-07-23T12:57:15.602Z",
-        updatedAt: "2024-07-23T13:00:14.983Z",
-        __v: 0,
-      },
-      {
-        _id: "668543acf1589a11e4d26efd",
-        productStatus: "PROCESS",
-        productCollection: "DRINK",
-        productName: "Juice",
-        productPrice: 10,
-        productLeftCount: 20,
-        productSize: "NORMAL",
-        productVolume: "1",
-        productDesc: "The taste u",
-        productImages: [
-          "uploads/products/e91c2660-c510-46d3-8b1c-2713aab1b8d4.jpg",
-        ],
-        productViews: 1,
-        createdAt: "2024-07-03T12:27:24.663Z",
-        updatedAt: "2024-07-24T09:50:15.359Z",
-        __v: 0,
-      },
-    ];
-    // Slice:Data => Store
-    // @ts-ignore
-    setPopularDishes(result);
+    const product = new ProductService();
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "productViews",
+        productCollection: ProductCollection.DISH,
+      })
+      .then((data) => {
+        setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
   }, []);
   return (
     <div className={"homepage"}>
